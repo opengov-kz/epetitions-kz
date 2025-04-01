d58563d9c9abda0d199113d096f9ab2c09cc7343
Arman Myssanov <mysanov2004@gmail.com>
2025-04-01 23:08:48 +0500

    Optimize memory usage in comments_parser.py

--- scripts/comments_parser.py
+++ scripts/comments_parser.py
@@ -1,8 +1,17 @@
 import requests
 import json
 import csv
-import itertools
 from datetime import datetime
+import os
+import itertools
+import errno
+
+def silent_remove(filename):
+    try:
+        os.remove(filename)
+    except OSError as e:
+        if e.errno != errno.ENOENT:
+            raise
 
 class CommentsParser:
     def __init__(self, api_url="https://epetition.kz/api/public/v1/petitions"):
@@ -13,12 +22,14 @@ class CommentsParser:
         api_url -- API url для получения комментариев
         """
         self.api_url = api_url
+        self.timeout = 1
+        self.max_page_size = 1
         self.headers = {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
         }
-
+    
     def fetch_comments(self, petition_id):
         """
         Получение комментариев одной петиции из API
@@ -32,58 +43,56 @@ class CommentsParser:
                 try:
                     response = requests.get(f"{self.api_url}/{petition_id}/comments",
                                             params={"size":1000, "page":i},
-                                            headers=self.headers, timeout=3)
+                                            headers=self.headers,
+                                            timeout=self.timeout)
                     response.raise_for_status()
                     break
                 except requests.exceptions.Timeout:
                     continue
-                
-            comments.extend(response.json()["content"])
-            if response.json()["last"]: break
             
+            comments.extend(response.json()["content"])
+            if response.json()["last"]:
+                break
         return comments
-    
-    def fetch_all_comments(self):
+
+    def fetch_petition_list_page(self, size, page):
         """
-        Получение всех комментариев из API
+        Получение страницы списка петиций из API
+
+        Arguments:
+        size -- размер одной страницы списка
+        page -- номер страницы списка
         """
         while True:
             try:
                 response = requests.get(f"{self.api_url}/short",
-                                        params={"size":1},
-                                        headers=self.headers, timeout=2)
-                response.raise_for_status()
-                
-                response = requests.get(f"{self.api_url}/short",
-                                        params={"size":response.json()["totalElements"]},
-                                        headers=self.headers, timeout=2)
+                                        params={"size":size, "page":page},
+                                        headers=self.headers,
+                                        timeout=self.timeout)
                 response.raise_for_status()
                 break
-
             except requests.exceptions.Timeout:
                 continue
-            
-        all_comments = []
-        content = response.json()["content"]
-        for i, petition in enumerate(content, start=1):
-            print(f"Fetching comments from petition {i} of {len(content)}")
-            all_comments.extend(self.fetch_comments(petition["id"]))
-        
-        return all_comments
+        return response.json()
 
-    def save_to_csv(self, all_comments, csv_path):
+    def save_to_csv(self, comments, csv_path):
         """
-        Сохранение всех комментариев в CSV файл
+        Сохранение комментариев в CSV файл
 
         Arguments:
-        all_comments -- list всех комментариев в формате json
+        comments -- list комментариев в формате json
         csv_path -- Путь к CSV файлу
         """
-        field_names = all_comments[0].keys()
-        with open(csv_path, 'w', newline='', encoding='utf-8') as csv_file:
+        if len(comments) == 0:
+            return
+        field_names = comments[0].keys()
+        
+        with open(csv_path, 'a', newline='', encoding='utf-8') as csv_file:
             writer = csv.DictWriter(csv_file, fieldnames=field_names)
-            writer.writeheader()
-            writer.writerows(all_comments)
+
+            if os.path.getsize(csv_path) == 0:
+                writer.writeheader()
+            writer.writerows(comments)
 
     def run(self, csv_path):
         """
@@ -92,10 +101,20 @@ class CommentsParser:
         Arguments:
         csv_path -- Путь к CSV файлу
         """
-        all_comments = self.fetch_all_comments()
-        self.save_to_csv(all_comments, csv_path)
-        return all_comments
+        silent_remove(csv_path)
+        try:
+            for i in itertools.count():
+                petition_list_page = self.fetch_petition_list_page(self.max_page_size, i)
 
+                for short_petition in petition_list_page["content"]:
+                    comments = self.fetch_comments(short_petition["id"])
+                    self.save_to_csv(comments, csv_path)
+
+                if petition_list_page["last"]:
+                    break
+        except:
+            silent_remove(csv_path)
+            raise
 
 if __name__ == "__main__":
     import argparse
